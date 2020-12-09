468a8705a416c07e0e714af09222b1d837e99f2b
Nguyen Thien Tu <15050790+thientu995@users.noreply.github.com>
2020-12-09 15:00:24 +0700

    remove

--- Ourlife/ClientApp/src/app/pages/horizontal-timeline/horizontal-timeline.module.ts
+++ Ourlife/ClientApp/src/app/pages/horizontal-timeline/horizontal-timeline.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HorizontalTimelineComponent } from './horizontal-timeline.component';
+// import { HorizontalTimelineComponent } from './horizontal-timeline.component';
 
 
 @NgModule({
